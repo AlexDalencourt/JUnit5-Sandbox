b099f165aecdf3ec11ce2d2a44676919ff0fcfb1
adalencourt <adalencourt@norauto.com>
2022-05-01 20:07:51 +0200

    sources

---
+++ src/main/java/junit5/Calculator.java
@@ -0,0 +1,16 @@
+package junit5;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int value, int divisor) {
+        return value / divisor;
+    }
+}
---
+++ src/test/java/junit5/disable/DisabledTestsDemo.java
@@ -0,0 +1,17 @@
+package junit5.disable;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DisabledTestsDemo {
+
+    @Disabled("Disabled until bug #42 has been resolved")
+    @Test
+    void testWillBeSkipped() {
+    }
+
+    @Test
+    void testWillBeExecuted() {
+    }
+
+}
